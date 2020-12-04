8e78822145d29b52a3c91266917171419d5da02a
k-souvatzidaki <kwn.souv@gmail.com>
2020-12-04 20:04:34 +0200

    add example (Mini)Python file

---
+++ example.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+#A miniPython example
+
+def fib(n, a):    # write Fibonacci series up to n
+               a = 0
+               b = 1
+               while a < n:
+                        print a
+                        a = b
+                        b = a + list[a]
+                      
+def funcwithdef(name,university="aueb"):
+       print name, " studies in ", university
+	   print fib(3,1) ** 3 / (8 + 3)
