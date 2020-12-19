c893f5f52cb64009d722ab49c51f7592b6944f6b
lydia-ath <lydia_athanasiou@hotmail.gr>
2020-12-19 13:55:39 +0200

    fix productions

--- example.py
+++ example.py
@@ -7,8 +7,9 @@ def fib(n, a):    # write Fibonacci series up to n
                while a < n:
                         print a
                         a = b
-                        b = a + list[a]
+                        b = a + list[a + 1]
                       
 def funcwithdef(name,university="aueb"):
        print name, " studies in ", university
-	   print fib(3,1) ** 3 / (8 + 3)
+	   print fib(3,1) ** 3 / (8 - 3 + 7)
+       if not not a < 5 and not a == 6 : print a
