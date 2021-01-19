683014a832fcecbd4183c3293a04c7256cd09920
k-souvatzidaki <kwn.souv@gmail.com>
2021-01-19 18:17:45 +0200

    Arguments check fixed

--- Visitor.java
+++ Visitor.java
@@ -5,135 +5,137 @@ import java.util.*;
 public class Visitor extends DepthFirstAdapter {
 
 	private Hashtable <String,ArrayList<Node>> symtable;	
+	static int errors;
 
 	Visitor(Hashtable symtable) {
 		this.symtable = symtable;
+		errors = 0;
 	}
 	
 	@Override
 	public void caseAFunction(AFunction node) {
-
-		System.out.println("\nInside caseAFunction");
-
-        inAFunction(node);
+		inAFunction(node);
         if(node.getId() != null) {
             node.getId().apply(this);
         }
         {
 			Object temp[] = node.getArgument().toArray();
-
 			String function_name = node.getId().toString();
 			int line = ((TId) node.getId()).getLine();
-			ArrayList<Node> temp1 = symtable.get(function_name);
+			//calculate total # of arguments of function "node"
+			int node_params = 0; int default_node_params = 0;
+			if (((AFunction) node).getArgument().size() == 1) {
+				AArgument args_node = (AArgument) ((AFunction) node).getArgument().get(0);
+				//add the first argument
+				node_params = 1;
+				default_node_params = args_node.getAssignV().size();
+				//add the rest of the arguments
+				node_params += args_node.getCommaAssign().size();
+				for (Object v  : args_node.getCommaAssign()) {
+					default_node_params += ((ACommaAssign) v).getAssignV().size();
+				}
+			}
+			//check if there is a function with the same name
 			boolean flag = true;
-			if (temp1 != null) {
-				for(Node n : temp1) {
+			if(symtable.containsKey(function_name)) {
+				ArrayList<Node> temp_nodes = symtable.get(function_name);
+				Node n;
+				//check if the # of arguments is different
+				for(int k = 0; k < temp_nodes.size(); k++) {
+					n = temp_nodes.get(k);
 					if(n instanceof AFunction) {
-						AArgument args_n = (AArgument) ((AFunction) n).getArgument().get(0);
-						int size_n = 0; // # of all parameters (including default)
-						int size_default = 0; //# of default parameters
+						//calculate total # of arguments of function "n"
+						int n_params = 0; int default_n_params = 0;
 						if (((AFunction) n).getArgument().size() == 1) {
-							size_n = 1;
-							size_default = args_n.getAssignV().size();
+							AArgument args_n = (AArgument) ((AFunction) n).getArgument().get(0);
+							//add the first argument
+							n_params = 1;
+							default_n_params = args_n.getAssignV().size();
+							//add the rest of the arguments
+							n_params += args_n.getCommaAssign().size();
 							for (Object v  : args_n.getCommaAssign()) {
-								size_default += ((ACommaAssign) v).getAssignV().size();
+								default_n_params += ((ACommaAssign) v).getAssignV().size();
 							}
-							size_n += args_n.getCommaAssign().size();
 						}
-						AArgument args_node = (AArgument) ((AFunction) node).getArgument().get(0);
-						int size_node=0; // # of all parameters (including default)
-						int default_node = 0; //# of default parameters
-						if (((AFunction) node).getArgument().size() == 1) {
-							size_node = 1;
-							default_node = args_node.getAssignV().size();
-							// minus the default
-							for (Object v  : args_node.getCommaAssign()) {
-								default_node += ((ACommaAssign) v).getAssignV().size();
-							}
-							size_node += args_node.getCommaAssign().size();
-						}
-						
-						if (((size_n - size_default > size_node - default_node && default_node==0) || (size_node - default_node > size_n))) {
-							
-						} 
-						else {
+						if (!((n_params- default_n_params > node_params - default_node_params && default_node_params==0) || (node_params - default_node_params > n_params))) {
 							System.out.println("Line " + line + ": " +" Function " + function_name +" is already defined");
+							errors++;
 							flag = false;
-						}
+							break;
+						} 
 					}
 				}
 			}
-			if (flag){
-				symtable.put(function_name,new ArrayList<Node>(Arrays.asList(node)));
+			if (flag) {
+				//add function in table
+				if(!symtable.containsKey(function_name)) symtable.put(function_name,new ArrayList<Node>(Arrays.asList(node)));
+				else symtable.get(function_name).add(node);
+				//proceed to check arguments
 				for(int i = 0; i < temp.length; i++) {
 					((PArgument) temp[i]).apply(this);
 				}
+				//proceed to check statement
 				if(node.getStatement() != null) {
 					node.getStatement().apply(this);
 				}
 			}
 		}
-		
         outAFunction(node);
     }
 
 
-	@Override
-	public void inAArgument(AArgument node) {
-
-		System.out.println("Inside inAArgument");
-
-		String arg_name = node.getId().toString();
-		//the first argument is never a duplicate
-		
-		if(!symtable.containsKey(arg_name)) symtable.put(arg_name,new ArrayList<Node>(Arrays.asList(node)));
-		else symtable.get(arg_name).add(node);
-		//the rest of the arguments are "CommaAssign"s, checked in "inACommaAssign(..)"
-	}
-	
-	// TODO : check duplicated arguments in different functions with the same name (same name, different # of arguments (without default))
-	@Override
-	public void inACommaAssign(ACommaAssign node) {
-
-		System.out.println("Inside inACommaAssign");
-
-		String arg_name = node.getId().toString(); //get name of the argument
-		String function_name = ((AFunction)(node.parent().parent())).getId().toString(); //get name of the function
-		//AFunction function_name_o = (AFunction) node.parent().parent();
-		int line = ((TId) node.getId()).getLine();
-		//if no symbol with the same name exists
-		if(!symtable.containsKey(arg_name)) symtable.put(arg_name,new ArrayList<Node>(Arrays.asList(node)));
-		else {
-
-			//get all the symbols with the same name 
-			ArrayList<Node> temp = symtable.get(arg_name);
-			//check if a symbol with type AArgument or ACommaAssign with the same name exists
+	public void caseAArgument(AArgument node) {
+        inAArgument(node);
+        if(node.getId() != null) {
+            node.getId().apply(this);
+        }
+        {
+            Object temp[] = node.getAssignV().toArray();
+            for(int i = 0; i < temp.length; i++) {
+                ((PAssignV) temp[i]).apply(this);
+            }
+		}
+		{
+			//check for duplicate argument names in the same function
+			String arg_name = node.getId().toString();
+			ArrayList<String> arguments = new ArrayList<String>(Arrays.asList(arg_name));
+			Object temp[] = node.getCommaAssign().toArray();
 			boolean flag = true;
-			for(int i =0; i < temp.size(); i++) {
-				Node n = temp.get(i);
-				if(n instanceof AArgument) {
-					String n_function = ((AFunction)(n.parent())).getId().toString();
-					//AFunction n_function = (AFunction) n.parent();
-					System.out.println("Parent of AArgument = "+ n_function);
-					if(n_function.equals(function_name)) {
-					//if(isEqualFunc(n_function, function_name_o)){
-						System.out.println("Line " + line + ": " +"Duplicate argument " + arg_name +" in function "+function_name);
+			int line = ((TId) node.getId()).getLine();
+			String function_name = ((AFunction)(node.parent())).getId().toString();
+			for(int i = 0; i < temp.length; i++) {
+				String temp_id = ((ACommaAssign)temp[i]).getId().toString();
+				int size = arguments.size();
+				for(int k = 0; k < size; k++) {
+					if(arguments.get(k).equals(temp_id)) {
+						System.out.println("Line " + line + ": " +"Duplicate argument " + temp_id +" in function "+function_name);
 						flag = false;
-					}
-				} else if(n instanceof ACommaAssign) {
-					String n_function = ((AFunction)(n.parent().parent())).getId().toString();
-					//AFunction n_function = (AFunction) n.parent().parent();
-					System.out.println("Parent of ACommaAssign = "+ n_function);
-					if(n_function.equals(function_name)) {
-					//if(isEqualFunc(n_function, function_name_o)){
-						System.out.println("Line " + line + ": " +"Duplicate argument " + arg_name +" in function "+function_name);
-						flag = false;
-					}
+						errors++;
+						break;
+					} else arguments.add(temp_id);
 				}
 			}
-			if(flag == true) symtable.get(arg_name).add(node);
-
+		
+			//if no duplicates, add arguments in symbol table, else print error
+			if(flag) {
+				//add first argument in symbol table
+				if(!symtable.containsKey(arg_name)) symtable.put(arg_name,new ArrayList<Node>(Arrays.asList(node)));
+				else symtable.get(arg_name).add(node);
+				//add the rest arguments in symbol table
+				for(int i = 0; i < temp.length; i++) {
+					((PCommaAssign) temp[i]).apply(this);
+				}
+			}
 		}
+        outAArgument(node);
+    }
+
+	//we have checked for duplicates in the same function in caseAArgument. just add the arguments in the table
+	@Override
+	public void inACommaAssign(ACommaAssign node) {
+		String arg_name = node.getId().toString(); //get name of the argument
+		if(!symtable.containsKey(arg_name)) symtable.put(arg_name,new ArrayList<Node>(Arrays.asList(node)));
+		else symtable.get(arg_name).add(node);
 	}
 	
 	// MAYBE for identifier in identifier: statement 
@@ -145,14 +147,8 @@ public class Visitor extends DepthFirstAdapter {
 	@Override
 	public void inAAssignStatement(AAssignStatement node) {
 		String var_name = node.getId().toString();
-		
 		if(!symtable.containsKey(var_name)) symtable.put(var_name,new ArrayList<Node>(Arrays.asList(node)));
 		else symtable.get(var_name).add(node);
-
-		/*
-		if(!symtable.contains(var_name)) symtable.put(var_name,node);
-		else if (!symtable.get(fname) instanceof ACommaAssign ) symtable.put(fname,node);  //!!!!!!!!!!!!!!!
-		*/
 	}
 	
 	/*@Override  // list[a] = 
