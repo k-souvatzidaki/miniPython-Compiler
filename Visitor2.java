176b0ed501c958eec3a48da0be95bcd38b3c3c8a
lydia-ath <lydia_athanasiou@hotmail.gr>
2021-01-27 17:54:41 +0200

    argument values in function call

--- Visitor2.java
+++ Visitor2.java
@@ -6,13 +6,25 @@ public class Visitor2 extends DepthFirstAdapter {
 
 	private Hashtable <String,ArrayList<Node>> symtable;	
 	int errors; Node fun;
-	String return_type; boolean in_function;
+	String return_type; boolean in_function; boolean in_function_call; boolean in_func_declaration;
+	ArrayList<String> real_arguments = null; 
+	ArrayList<String> passed_arguments_types = null;
 
 	Visitor2(Hashtable symtable) {
 		this.symtable = symtable;
 		errors = 0;
 		in_function = false;
-    }
+		in_function_call = false;
+	}
+	
+	@Override
+	public void inAFunction(AFunction node) {
+		in_func_declaration = true;
+	}
+	@Override
+	public void outAFunction(AFunction node) {
+		in_func_declaration = false;
+	}
 	
 	/** Check if a called function is defined with the right amount of argumentscall non defined function. */
 	@Override
@@ -22,14 +34,23 @@ public class Visitor2 extends DepthFirstAdapter {
             node.getId().apply(this);
 		} 
 		fun = null; return_type = null;
+		ArrayList<Node> passed = new ArrayList<Node>();
+		ArrayList<String> real = new ArrayList<String>();
+		ArrayList<String> passed_types = new ArrayList<String>();
         {
 			//get id
 			String id = node.getId().toString();
 			//get # of arguments
 			int params = node.getArglist().size();
+			//add passed arguments in arraylist
 			if(params == 1) {
+				passed.add(((AArglist)node.getArglist().get(0)).getExpression()); 
 				params+= ((AArglist)node.getArglist().get(0)).getCommaExp().size();
+				for(int i = 0; i < params-1; i++) {
+					passed.add(((ACommaExp)((AArglist)node.getArglist().get(0)).getCommaExp().get(i)).getExpression());
+				}
 			}
+			
 			//get line
 			int line = ((TId) node.getId()).getLine();
 
@@ -39,9 +60,12 @@ public class Visitor2 extends DepthFirstAdapter {
 				errors++;
 			} else {
 				ArrayList<Node> nodes = symtable.get(id);
+				HashMap<String,PValue> real_default;
 				boolean flag = false; boolean enough_args = false;
 				for(int i =0; i< nodes.size(); i++ ) {
 					if( nodes.get(i) instanceof AFunction) {
+						real = new ArrayList<String>();
+						real_default = new HashMap<String,PValue>();
 						flag = true;
 						//count args (non-default and total)
 						AFunction n = (AFunction)nodes.get(i);
@@ -51,18 +75,41 @@ public class Visitor2 extends DepthFirstAdapter {
 							//add the first argument
 							n_params = 1;
 							default_n_params = args_n.getAssignV().size();
+							if(default_n_params==1) real_default.put(args_n.getId().toString(), (((AAssignV)args_n.getAssignV().get(0)).getValue())); 
+							real.add(args_n.getId().toString());
 							//add the rest of the arguments
 							n_params += args_n.getCommaAssign().size();
 							for (Object v  : args_n.getCommaAssign()) {
 								default_n_params += ((ACommaAssign) v).getAssignV().size();
+								if(((ACommaAssign) v).getAssignV().size()==1) real_default.put(((ACommaAssign)v).getId().toString(), (((AAssignV)((ACommaAssign)v).getAssignV().get(0)).getValue()));
+							    real.add(((ACommaAssign)v).getId().toString());
 							}
 						}
 						if((default_n_params==0 && params==n_params) 
 						|| (params >= n_params-default_n_params && n_params >= params)) {
 							enough_args = true;
 							fun = ((AFunction)nodes.get(i));
+							while (real.size() > passed.size()){
+								passed.add(real_default.get(real.get(passed.size())));
+							}
+							//convert passed arguments to their types
+							String type = null;
+							for(int k = 0; k < passed.size(); k++) {
+								if(passed.get(k) instanceof PExpression) type = getExpressionType(((PExpression)passed.get(k)));
+								else {
+									PValue val = ((PValue)passed.get(k));
+									if(val instanceof ANumValue) type = "NUMBER";
+									else if (val instanceof ANoneValue) type ="NONE";
+									else if (val instanceof AStringValue) type = "STRING";
+								}
+								passed_types.add(type);
+							}
+							
 							break;
 				}}}
+				System.out.println("passed_arguments "+passed);
+				System.out.println("passed_arguments types "+passed_types);
+				System.out.println("real_arguments "+real);
 				if(!flag) { 
 					System.out.println("Line " + line + ": " +"Function " + id +" is not defined");
 					errors++;
@@ -78,10 +125,63 @@ public class Visitor2 extends DepthFirstAdapter {
 			}
 		}
 		if(fun!=null) {
+			real_arguments = real;
+			passed_arguments_types = passed_types;
+			in_function_call = true;
 			((AFunction)fun).apply(this); //to find return type
+			in_function_call = false;
+			real_arguments = null;
+			passed_arguments_types = null;
 		}
         outAFunctionCall(node);
 	}
+
+	//find the type of an id variable
+	public String getExpressionType(PExpression expression) {
+		if (expression instanceof AAddExpression || expression instanceof AMinExpression || expression instanceof AMultExpression 
+		    || expression instanceof AMultmultExpression || expression instanceof AModExpression || expression instanceof ADivExpression 
+			|| expression instanceof AParExpression || expression instanceof AMinExpression || expression instanceof AMaxExpression){
+			return "NUMBER";
+		}else if(expression instanceof AValueExpression) {
+			PValue val = ((AValueExpression)expression).getValue();
+			if(val instanceof ANumValue) return "NUMBER";
+			else if (val instanceof ANoneValue) return "NONE";
+			else if (val instanceof AStringValue) return "STRING";
+		}else if(expression instanceof ATypeExpression) return  "TYPE";
+		else if(expression instanceof AOpenExpression) return "OPEN";
+		else if(expression instanceof AListConExpression) {
+			System.out.println("Line " + ": " +"Invalid Syntax");
+			errors++;
+		}else if(expression instanceof AListexpExpression ) {
+			String id = ((AListexpExpression)expression).getId().toString();
+			//if it's a global variable
+			ArrayList<Node> nodes = symtable.get(id); AAssignStatement n = null;
+			int line = ((AListexpExpression)expression).getId().getLine(); int other_line;
+			for(int i = 0; i < nodes.size(); i++) {
+				if(nodes.get(i) instanceof AAssignStatement){
+					other_line = ((AAssignStatement)nodes.get(i)).getId().getLine();
+					if(other_line > line) break;
+					else n = (AAssignStatement)nodes.get(i);
+				}
+			}
+			return (String)getOut(n);
+		}
+		else if(expression instanceof AIdExpression) {
+			String id = ((AIdExpression)expression).getId().toString();
+			//if it's a global variable
+			ArrayList<Node> nodes = symtable.get(id); AAssignStatement n = null;
+			int line = ((AIdExpression)expression).getId().getLine(); int other_line;
+			for(int i = 0; i < nodes.size(); i++) {
+				if(nodes.get(i) instanceof AAssignStatement){
+					other_line = ((AAssignStatement)nodes.get(i)).getId().getLine();
+					if(other_line > line) break;
+					else n = (AAssignStatement)nodes.get(i);
+			}}
+			return (String)getOut(n);
+		}
+		return null;
+	}
+
 	
 
 	/** Operations typechecking for variables and function calls */ 
@@ -91,10 +191,21 @@ public class Visitor2 extends DepthFirstAdapter {
 		System.out.println("HERE");
 		inAAddExpression(node);
 		int other_line=0; String type = null; AAssignStatement n; ArrayList<Node> nodes; int line;
+		ArrayList<String> real = null; ArrayList<String> passed = null;
         if(node.getLpar() != null) {
 			Node left = node.getLpar();
-			if(left instanceof AFuncCallExpression) in_function = true;
+			if(left instanceof AFuncCallExpression) {
+				in_function = true;
+			}
 			left.apply(this);
+			if(in_function_call) {
+				//deep copy arguments arrays
+				System.out.println("MPLOU");
+				real = new ArrayList<String>();
+				for(String s : real_arguments) real.add(s);
+				passed = new ArrayList<String>();
+				for(String no : passed_arguments_types) passed.add(no);
+			}
 			if(!(left instanceof AAddExpression || left instanceof AMinExpression || left instanceof AMultExpression 
 				|| left instanceof AMultmultExpression || left instanceof AModExpression || left instanceof ADivExpression 
 				|| left instanceof AParExpression)) {
@@ -111,22 +222,30 @@ public class Visitor2 extends DepthFirstAdapter {
 					}
 				}else if(left instanceof AIdExpression) {
 					String id = ((AIdExpression)left).getId().toString();
-					System.out.println("a");
+					//System.out.println("a");
 					nodes = symtable.get(id); n = null;
 					System.out.println("ou");
 					line = ((AIdExpression)left).getId().getLine();
-					for(int i = 0; i < nodes.size(); i++) {
-						if(nodes.get(i) instanceof AAssignStatement) {
-							other_line = ((AAssignStatement)nodes.get(i)).getId().getLine();
-							if(other_line > line) break;
-							else n = (AAssignStatement)nodes.get(i);
-							type = (String)getOut(n);
-						}else{
-							//TODO: if instanceof Argument
-							type = "ARGUMENT";
+					if(in_function_call) {
+						System.out.println("IN FUNCTION CALL");
+						int index = real.indexOf(id);
+						type = passed.get(index);
+					}
+					if (type==null){
+						System.out.println("TYPE IS NULL");
+						for(int i = 0; i < nodes.size(); i++) {
+							if(nodes.get(i) instanceof AAssignStatement){
+								other_line = ((AAssignStatement)nodes.get(i)).getId().getLine();
+								if(other_line > line) break;
+								else n = (AAssignStatement)nodes.get(i);
+								type = (String)getOut(n);
+							}
 						}
 					}
-					System.out.println(type);
+					if(in_func_declaration && real_arguments == null) {
+						type = "NUMBER";
+						System.out.println("TYPE IS STILL NULL");
+					}
 					
 					if(type.equals("NONE")){
 						System.out.println("Line " + ": " +"Add operation cannot be done on None");
@@ -160,8 +279,17 @@ public class Visitor2 extends DepthFirstAdapter {
 
 		if(node.getRpar() != null) {
 			Node right = node.getRpar();
-			if(right instanceof AFuncCallExpression) in_function = true;
+			if(right instanceof AFuncCallExpression) {
+				in_function = true;
+			}
 			right.apply(this);
+			if(in_function_call) {
+				//deep copy arguments arrays
+				real = new ArrayList<String>();
+				for(String s : real_arguments) real.add(s);
+				passed = new ArrayList<String>();
+				for(String no : passed_arguments_types) passed.add(no);
+			}
 			if(!(right instanceof AAddExpression || right instanceof AMinExpression || right instanceof AMultExpression 
 				|| right instanceof AMultmultExpression || right instanceof AModExpression || right instanceof ADivExpression
 				|| right instanceof AParExpression)) {
@@ -180,18 +308,26 @@ public class Visitor2 extends DepthFirstAdapter {
 					String id = ((AIdExpression)right).getId().toString();
 					nodes = symtable.get(id); n = null;
 					line = ((AIdExpression)right).getId().getLine();
-					for(int i = 0; i < nodes.size(); i++) {
-						if(nodes.get(i) instanceof AAssignStatement){
-							other_line = ((AAssignStatement)nodes.get(i)).getId().getLine();
-							if(other_line > line) break;
-							else n = (AAssignStatement)nodes.get(i);
-							type = (String)getOut(n);
-						}else{
-							//TODO: if instanceof Argument
-							type = "ARGUMENT";
+					if(in_function_call) {
+						System.out.println("IN FUNCTION CALL");
+						int index = real.indexOf(id);
+						type = passed.get(index);
+					}
+					if (type==null){
+						System.out.println("TYPE IS NULL");
+						for(int i = 0; i < nodes.size(); i++) {
+							if(nodes.get(i) instanceof AAssignStatement){
+								other_line = ((AAssignStatement)nodes.get(i)).getId().getLine();
+								if(other_line > line) break;
+								else n = (AAssignStatement)nodes.get(i);
+								type = (String)getOut(n);
+							}
 						}
 					}
-					System.out.println(type);
+					if(in_func_declaration && real_arguments == null) {
+						type = "NUMBER";
+						System.out.println("TYPE IS STILL NULL");
+					}
 					
 					if(type.equals("NONE")){
 						System.out.println("Line " + ": " +"Add operation cannot be done on None");
