47598307ee5fe848a7f513c588de54b42e62e308
k-souvatzidaki <kwn.souv@gmail.com>
2021-01-30 14:47:48 +0200

    Add python file with errors

--- example.py
+++ example.py
@@ -1,15 +1,22 @@
-#! /usr/bin/env python
-#A miniPython example
+def add(x,y=2):
+   return x + y
+print add(1)
 
-def fib(n, a):    # write Fibonacci series up to n
-               a = 0
-               b = 1
-               while a < n:
-                        print a
-                        a = b
-                        b = a + list[a + 1]
-                      
-def funcwithdef(name,university="aueb"):
-       print name, " studies in ", university
-	   print fib(3,1) ** 3 / (8 - 3 + 7)
-       if not not a < 5 and not a == 6 : print a
+#def add(x) : print x + "String"
+#def add(x,y,z=1) : return x
+
+x = 1
+x="hello world"
+
+print 1
+print x+2
+
+def add2(x,y):
+   return x + y
+k="hello world"
+print add2(2,k)
+
+def add3(x,y):
+   return "hello world"
+print add3(2,1)+2
+
